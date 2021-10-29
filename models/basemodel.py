55e3d86c77da3584d0a26bec86a5bdd0dc6bbce5
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-29 17:42:13 -0400

    Update basemodel.py

--- models/basemodel.py
+++ models/basemodel.py
@@ -5,6 +5,18 @@ import uuid
 from uuid import uuid4
 from datetime import datetime
 
+# Classes are real-world objects
+# I like cars, so think of a car
+# In the class, there are different pieces of data that 
+# make up the wheels, suspension, engine, etc.
 
-class BaseModel:
-    """ Let's get things started... """
+class BaseModel(object):
+    """ Initializing the base class """
+
+    def __init__(self, *args, **kwargs):
+        """ Start building the basemodel """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    
