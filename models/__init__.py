f1ae5e2386dc89efc45900bd0f312f682e1f9127
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-11-02 14:36:36 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """ This is the python equivalent to header files """
-from models.engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage # THIS LINE 
 storage = FileStorage()
 storage.reload()
