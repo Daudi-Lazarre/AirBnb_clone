849ea9455df4b4e5d8ac5d4ba81a05d471ea96a2
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-11-04 18:56:40 -0400

    Create console.py

---
+++ console.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Console the pain of working alone
+"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Quit and help implementations
+    """
+
+    prompt = "(hbnb)"
+
+def do_quit(self, *args):
+    """
+    Exit the terminal
+    """
+    return True
+
+def do_EOF(self, arg):
+    """
+    End of file (ctrl + d)
+    """
+    return True
+
+def empty_line(self):
+    pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
