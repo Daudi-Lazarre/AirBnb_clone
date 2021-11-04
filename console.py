cdcc89b57e1d9ff232aa17c3ea87b3d55aa92135
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-11-04 19:33:49 -0400

    Update console.py

--- console.py
+++ console.py
@@ -2,8 +2,8 @@
 """
 Console the pain of working alone
 """
-import cmd
 
+import cmd
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -12,7 +12,7 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb)"
 
-    def do_quit(self, *args):
+    def do_quit(self, arg):
         """
         Exit the terminal
         """
