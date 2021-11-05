4782d364454f7f781f428795e1a13fdd1aab52b1
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-11-04 23:11:42 -0400

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
 """
     City file for console
 """
+
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """City class"""
+
+    state_id = ""
+    name = ""
