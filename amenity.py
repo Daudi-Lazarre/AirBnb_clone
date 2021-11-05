c49e7ec66df7ae905d55e50f51d033816830b0d5
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-11-04 23:11:45 -0400

    Update amenity.py

--- amenity.py
+++ amenity.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
 """
     amenity file for console
 """
+
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Amenity class"""
+
+    name = ""
